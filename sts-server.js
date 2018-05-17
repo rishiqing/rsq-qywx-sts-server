a05b66b0ecb693bab3afc9446b4374532a68f75f
Wallace Mao <maowenqiang0752@163.com>
2018-05-17 14:30:59 +0800

    add customer kanban url

--- sts-server.js
+++ sts-server.js
@@ -3,7 +3,7 @@ var chalk = require('chalk');
 var STS = require('ali-oss').STS;
 var co = require('co');
 var fs = require('fs');
-var secret = require('../secret/index')
+var secret = require('./secret/index')
 var app = express();
 
 function getProlicy (path, strUser) {
@@ -30,10 +30,16 @@ var client = new STS({
   accessKeySecret: secret.aliOSS.accessKeySecret,
 });
 var ossRegion = 'oss-cn-beijing'
+//  默认的bucket，文件附件位于这个bucket
 var ossBucket = 'rishiqing-file'
+//  图片的bucket
+var ossImageBucket = 'rishiqing-images'
+//  默认的bucket中的根目录
 var ossRootPath = 'dingtalk/'
+//  自定义的封面的目录
+var ossCustomerCoverImagePath = 'cover/custom/'
 var roleArn = secret.aliOSS.roleArn
-var sessionName = 'dingtalkAppUser'
+var sessionName = 'qywxAppUser'
 var expiration = 900
 var port = 8300
 
@@ -69,6 +75,34 @@ app.get('/sts/:userId', function (req, res) {
     console.log(err);
     res.status(400).json(err.message);
   });
+});
+
+app.get('/sts/cover/custom/kanban/:userId', function (req, res) {
+    var userId = req.params.userId
+    if (!userId) {
+        return res.status(400)
+    }
+    //  公共读写，不使用userId
+    var policy = getProlicy(ossImageBucket + '/' + ossCustomerCoverImagePath + 'kanban/', '')
+
+    co(function* () {
+        var result = yield client.assumeRole(roleArn, policy, expiration, sessionName);
+        console.log(result);
+
+        res.set('Access-Control-Allow-Origin', '*');
+        res.set('Access-Control-Allow-METHOD', 'GET');
+        res.json({
+            AccessKeyId: result.credentials.AccessKeyId,
+            AccessKeySecret: result.credentials.AccessKeySecret,
+            SecurityToken: result.credentials.SecurityToken,
+            Expiration: result.credentials.Expiration
+        });
+    }).then(function () {
+        // pass
+    }).catch(function (err) {
+        console.log(err);
+        res.status(400).json(err.message);
+    });
 });
 
 app.listen(port, function () {
